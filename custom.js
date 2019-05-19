fb2d45ce6fc25b409fbbb3f57267483ead5b7560
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-05-19 19:37:19 +0900

    html homepages projects
    test

---
+++ custom.js
@@ -0,0 +1,1 @@
+﻿$(document).ready(function(){   $(".btnMenu").on("click",function(){ $(this).fadeOut(); $("section").addClass("on"); $("nav").addClass("on"); });   $("nav li").on("click",function(){ $(".btnMenu").fadeIn(); $("section").removeClass("on"); $("nav").removeClass("on"); var i = $(this).index(); $("section>div").removeClass("on"); $("section>div").eq(i).addClass("on"); });   });
